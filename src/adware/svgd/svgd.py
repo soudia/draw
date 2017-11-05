7089c3ca1efe4bff6cf7c3a33916c979e71ed2b0
Ousmane Dia <ousmane@elementai.com>
2017-11-05 13:24:02 -0500

    integrated vae_svgd in draw

---
+++ src/adware/svgd/svgd.py
@@ -0,0 +1,74 @@
+""" Stein Variational Gradient Descent
+    Reference paper: https://arxiv.org/pdf/1608.04471.pdf
+"""
+
+
+import numpy as np
+from scipy.spatial.distance import pdist, squareform
+
+
+# pylint: disable=W0622,C0325,E1101,C0103
+class SVGD(object):
+    """ Stein Variational Gradient Descent """
+    def __init__(self):
+        pass
+
+    def svgd_kernel(self, theta, h=-1):
+        """ Stein Variational Gradient Descent Kernel
+            Arg:
+                theta: Particles (consisting of weights, biais, etc.)
+                h: Radial Basis Kernel term (h) value (in this case it's
+                   the standard deviation)
+        """
+        sq_dist = pdist(theta)
+        pairwise_dists = squareform(sq_dist)**2  # pairwise distance in matrix form
+        if h < 0:  # if h < 0, using median trick
+            h = np.median(pairwise_dists)
+            h = np.sqrt(0.5 * h / np.log(theta.shape[0]+1))
+
+        # Compute the rbf kernel: exp(-||X_i - X_j|| / (2 * h^2)) for all i, j
+        Kxy = np.exp(-pairwise_dists / h**2 / 2)
+
+        dxkxy = -np.matmul(Kxy, theta)  # derivative of the kernel Kxy with respect to x
+        sumkxy = np.sum(Kxy, axis=1)
+
+        # Check the paper for more context https://arxiv.org/pdf/1608.04471.pdf Delta log(p)
+        for i in range(theta.shape[1]):
+            dxkxy[:, i] = dxkxy[:, i] + np.multiply(theta[:, i], sumkxy)
+        dxkxy = dxkxy / (h**2)
+        return (Kxy, dxkxy)
+
+    def update(self, x0, lnprob, n_iter=1000, stepsize=1e-3, bandwidth=-1, alpha=0.9, debug=False):
+        # Check input
+        if x0 is None or lnprob is None:
+            raise ValueError('x0 or lnprob cannot be None!')
+
+        theta = np.copy(x0)
+
+        # TODO: Try Adam here using tensorflow
+
+        # TODO: Mean-center theta, and compute the mean
+
+        # TODO: When updating theta, make sure to re-add the mean. Technically, theta as it is should
+        # but we never know.
+
+        # adagrad with momentum
+        fudge_factor = 1e-6
+        historical_grad = 0
+        for iter in range(n_iter):
+            if debug and (iter+1) % 1000 == 0:
+                print('iter ' + str(iter+1))
+
+            lnpgrad = lnprob(theta)
+            # calculating the kernel matrix
+            kxy, dxkxy = self.svgd_kernel(theta, h=-1)
+            grad_theta = (np.matmul(kxy, lnpgrad) + dxkxy) / x0.shape[0]
+
+            # adagrad (this in fact is adadelta an extension of adagrad)
+            if iter == 0:
+                historical_grad = historical_grad + grad_theta ** 2
+            else:
+                historical_grad = alpha * historical_grad + (1 - alpha) * (grad_theta ** 2)
+            adj_grad = np.divide(grad_theta, fudge_factor+np.sqrt(historical_grad))
+            theta = theta + stepsize * adj_grad
+        return theta
---
+++ src/adware/svgd/vae_svgd.py
@@ -0,0 +1,111 @@
+''' Variational Auto-encoder Stein Variational Gradient Descent '''
+
+from svgd.svgd import SVGD
+import tensorflow as tf
+
+
+# pylint: disable=E1129
+class VariationalEncSVGD(SVGD):
+    ''' Variational Auto-encoder Stein Variational Gradient Descent '''
+    def __init__(self, stepsize=1e-3, n_hidden=10, num_epoch=10):
+        ''' Initializer
+            Args:
+                stepsize:
+                n_hidden:
+                num_epoch:
+        '''
+        self.stepsize = stepsize
+        self.n_hidden = n_hidden
+        self.num_epoch = num_epoch
+
+        tf.set_random_seed(12345)
+
+        super(VariationalEncSVGD, self).__init__()
+
+    def update(self, theta, gradients, n_iter):
+        def dlnprob(theta):
+            return gradients
+
+        # theta = super(VariationalEncSVGD, self).update(theta, dlnprob, n_iter=n_iter)
+        return theta
+
+    def _init_weights(self, name, shape, stddev=0.1):
+        ''' Weight initialization '''
+        with tf.name_scope("%s_init" % name):
+            weights = tf.random_normal(shape, stddev=stddev)
+        return tf.Variable(weights, name)
+
+    def _forwardprop(self, X, w_1, w_2):
+        ''' Forward-propagation. '''
+        h = tf.tensordot(X, w_1, axes=[[1], [0]])
+        yhat = tf.tensordot(h, w_2, axes=[[1], [0]])
+        return yhat
+
+    def recognition_model(self, noisy_h_enc, particles, num_iter):
+        ''' Recognition model
+            Args:
+                h_enc:
+        '''
+        num_particles = particles.shape[1].value
+        dim_particle = particles.shape[2].value
+
+        w_1 = self._init_weights('w_1', (noisy_h_enc.shape[1].value,
+                                         self.n_hidden,
+                                         num_particles))
+
+        w_2 = self._init_weights('w_2', (self.n_hidden, dim_particle))
+
+        # X = tf.placeholder(tf.float32, shape=[noisy_h_enc.shape[0].value,
+        #                                       noisy_h_enc.shape[1].value],
+        #                    name='X')
+
+        # y = tf.placeholder(tf.float32, shape=[noisy_h_enc.shape[0].value,
+        #                                       num_particles,
+        #                                       dim_particle],
+        #                    name='y')
+
+        yhat = self._forwardprop(noisy_h_enc, w_1, w_2)
+
+        with tf.name_scope("metrics"):
+            cost = tf.reduce_mean(tf.squared_difference(particles, yhat), name='mse')
+
+        with tf.name_scope("optimization"):
+            optimizer = tf.train.AdamOptimizer(learning_rate=self.stepsize)
+            gradients = optimizer.compute_gradients(cost)
+            train_op = optimizer.apply_gradients(gradients, global_step=None, name='train_op')
+
+        # noise = self._init_weights('noise', [h_enc.shape[0].value, eta_dim])
+        # noisy_h_enc = tf.concat([h_enc, noise], axis=1)
+
+        # sess = tf.InteractiveSession()
+        # sess.run(tf.global_variables_initializer())
+
+        # for epoch in range(self.num_epoch):
+        #     _, loss = sess.run([train_op, cost],
+        #                        feed_dict={X: sess.run(noisy_h_enc),
+        #                                   y: sess.run(particles)})
+
+        #     print("Epoch = {}, recognition model loss = {:.7f}".format
+        #           (epoch + 1, 100. * loss))
+        #     print(sess.run(gradients,
+        #                    feed_dict={X: sess.run(noisy_h_enc),
+        #                               y: sess.run(particles)}))
+
+            particles = tf.tensordot(noisy_h_enc, w_1, axes=[[1], [0]])
+            particles = tf.tensordot(particles, w_2, axes=[[1], [0]])
+            particles = self.update(particles, gradients, num_iter)
+
+        # TODO Compute the mean, the logsigma, and the stdev
+        return particles, train_op
+
+
+if __name__ == '__main__':
+    x_1 = tf.random_normal([4, 3], stddev=0.01)
+    particles_ = tf.random_normal(shape=(x_1.shape[0].value,
+                                         10, 5), stddev=1.0)
+    vae_svgd = VariationalEncSVGD()
+    vae_svgd.recognition_model(x_1, particles_, 2)
+    # with tf.Session() as sess:
+    #     x_1 = tf.random_normal([4, 3], stddev=0.01)
+    #     sess.run(tf.global_variables_initializer())
+    #     vae_svgd.recognition_model(x_1, vae_svgd.dlnprob, 2, sess)
